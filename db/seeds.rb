e5534f5e9d60ab9590e4705d677f5aeec79e32a9
Micha Bernhard <mib70@pitt.edu>
2021-11-10 10:24:14 -0700

    Add plants to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@ turing_garden.plots.create!(number: 25, size: "Large", direction: "East")
 turing_garden.plots.create!(number: 26, size: "Small", direction: "West")
 library_garden.plots.create!(number: 2, size: "Small", direction: "South")
 other_garden.plots.create!(number: 738, size: "Medium", direction: "West")
+
+plant1 = Plant.create!(name: 'cucumber', description: 'is tasty', days_to_harvest: 10)
+plant2 = Plant.create!(name: 'tomato', description: 'is tasty', days_to_harvest: 10)
+plant3 = Plant.create!(name: 'pumpkin', description: 'is not tasty', days_to_harvest: 10)
+PlotPlant.create!(plot_id: turing_garden.id, plant_id: plant1.id)
+PlotPlant.create!(plot_id: turing_garden.id, plant_id: plant2.id)
+PlotPlant.create!(plot_id: other_garden.id, plant_id: plant3.id)
+
