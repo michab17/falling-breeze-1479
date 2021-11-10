33f1feadbe7f162bd90fbb69a5786ee5253506fe
Micha Bernhard <mib70@pitt.edu>
2021-11-10 10:54:37 -0700

    test extension

--- spec/features/garden/show_spec.rb
+++ spec/features/garden/show_spec.rb
@@ -18,9 +18,31 @@ RSpec.describe 'Garden show page' do
     PlotPlant.create!(plot_id: plot2.id, plant_id: plant5.id)
 
     visit garden_path(garden.id)
-    
+
     expect(page).to have_content('cucumber').once
     expect(page).to have_content('tomato').once
     expect(page).to_not have_content('pumkin')
+  end
+
+  it 'orders the list of plants by the amount they appear' do
+    garden = Garden.create!(name: 'Garden', organic: true)
+    plot1 = garden.plots.create!(number: 1, size: 'small', direction: 'east')
+    plot2 = garden.plots.create!(number: 2, size: 'small', direction: 'east')
+    plant1 = Plant.create!(name: 'cucumber', description: 'is tasty', days_to_harvest: 10)
+    plant2 = Plant.create!(name: 'tomato', description: 'is tasty', days_to_harvest: 10)
+    plant3 = Plant.create!(name: 'pumpkin', description: 'is not tasty', days_to_harvest: 101)
+    plant4 = Plant.create!(name: 'cucumber', description: 'is tasty', days_to_harvest: 10)
+    plant5 = Plant.create!(name: 'tomato', description: 'is tasty', days_to_harvest: 10)
+    plant6 = Plant.create!(name: 'cucumber', description: 'is tasty', days_to_harvest: 10)
+    PlotPlant.create!(plot_id: plot1.id, plant_id: plant1.id)
+    PlotPlant.create!(plot_id: plot1.id, plant_id: plant2.id)
+    PlotPlant.create!(plot_id: plot2.id, plant_id: plant3.id)
+    PlotPlant.create!(plot_id: plot2.id, plant_id: plant4.id)
+    PlotPlant.create!(plot_id: plot2.id, plant_id: plant5.id)
+    PlotPlant.create!(plot_id: plot2.id, plant_id: plant6.id)
+
+    visit garden_path(garden.id)
+
+    expect('cucumber').to appear_before('tomato')
   end
 end
