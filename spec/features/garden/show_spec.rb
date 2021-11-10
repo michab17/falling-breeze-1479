c6add1d1678874df97989981b070ba5028d24951
Micha Bernhard <mib70@pitt.edu>
2021-11-10 10:51:41 -0700

    Add more plants to test uniquness

--- spec/features/garden/show_spec.rb
+++ spec/features/garden/show_spec.rb
@@ -9,12 +9,16 @@ RSpec.describe 'Garden show page' do
     plant1 = Plant.create!(name: 'cucumber', description: 'is tasty', days_to_harvest: 10)
     plant2 = Plant.create!(name: 'tomato', description: 'is tasty', days_to_harvest: 10)
     plant3 = Plant.create!(name: 'pumpkin', description: 'is not tasty', days_to_harvest: 101)
+    plant4 = Plant.create!(name: 'cucumber', description: 'is tasty', days_to_harvest: 10)
+    plant5 = Plant.create!(name: 'tomato', description: 'is tasty', days_to_harvest: 10)
     PlotPlant.create!(plot_id: plot1.id, plant_id: plant1.id)
     PlotPlant.create!(plot_id: plot1.id, plant_id: plant2.id)
     PlotPlant.create!(plot_id: plot2.id, plant_id: plant3.id)
+    PlotPlant.create!(plot_id: plot2.id, plant_id: plant4.id)
+    PlotPlant.create!(plot_id: plot2.id, plant_id: plant5.id)
 
-    visit garden_path
-
+    visit garden_path(garden.id)
+    
     expect(page).to have_content('cucumber').once
     expect(page).to have_content('tomato').once
     expect(page).to_not have_content('pumkin')
