8f7ec0ce505f29374537f9f0dbf722fc58646748
Micha Bernhard <mib70@pitt.edu>
2021-11-10 10:32:15 -0700

    Add garden resources to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :gardens, only: [:show]
   resources :plots, only: [:index]
   resources :plot_plants, only: [:destroy]
   # get '/plots', to: 'plots#index', as: :plots
