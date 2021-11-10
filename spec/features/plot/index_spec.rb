7ffaf466325e3cebb62c5b17f9b15a0897348b2a
Micha Bernhard <mib70@pitt.edu>
2021-11-10 10:10:52 -0700

    Test user story 2

--- spec/features/plot/index_spec.rb
+++ spec/features/plot/index_spec.rb
@@ -35,4 +35,25 @@ RSpec.describe 'Plots index page' do
       expect(page).to have_content("pumpkin")
     end
   end
+
+  it 'allows you to remove a plant from a plot, without deleting the plant' do
+    garden = Garden.create!(name: 'Garden', organic: true)
+    plot1 = garden.plots.create!(number: 1, size: 'small', direction: 'east')
+    plot2 = garden.plots.create!(number: 2, size: 'small', direction: 'east')
+    plant1 = Plant.create!(name: 'cucumber', description: 'is tasty', days_to_harvest: 10)
+    plant2 = Plant.create!(name: 'tomato', description: 'is tasty', days_to_harvest: 10)
+    plant3 = Plant.create!(name: 'pumpkin', description: 'is not tasty', days_to_harvest: 10)
+    PlotPlant.create!(plot_id: plot1.id, plant_id: plant1.id)
+    PlotPlant.create!(plot_id: plot1.id, plant_id: plant2.id)
+    PlotPlant.create!(plot_id: plot2.id, plant_id: plant3.id)
+
+    visit plots_path
+
+    within "#id-#{plot1.id}" do
+      click_link "Remove #{plant1.name}"
+    end
+
+    expect(current_path).to eq(plots_path)
+    expect(page).to_not have_content(plant1.name)
+  end
 end
