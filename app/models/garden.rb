cd36fadf90376428f23c2536f9986ba8d2a01ebe
Micha Bernhard <mib70@pitt.edu>
2021-11-10 10:51:50 -0700

    Complete user story 3

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,9 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+  def plants
+    plots.joins(:plants)
+        .where("plants.days_to_harvest < 100")
+        .select("DISTINCT plants.name")
+  end
 end
